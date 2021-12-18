5e6829aa59dc241b6319f049a772588eb8934f62
bylorin <byxlorin@gmail.com>
2021-12-18 20:37:58 +0800

    fix log 宽度

--- src/views/tool/Log.vue
+++ src/views/tool/Log.vue
@@ -84,7 +84,7 @@
             prop="createTime"
             label="时间" sortable>
         </el-table-column>
-        <el-table-column label="操作" width="60">
+        <el-table-column label="操作" width="65">
           <template v-slot="scope">
             <el-tooltip effect="dark" content="删除" placement="top" :enterable="false">
               <el-popconfirm
